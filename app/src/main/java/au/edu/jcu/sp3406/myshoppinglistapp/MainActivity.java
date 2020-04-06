75cba3fb0d078067e6ab5719898a091063df2833
Danny <danny.connolly@my.jcu.edu.au>
2020-04-06 14:40:43 +1000

    added list

--- app/src/main/java/au/edu/jcu/sp3406/myshoppinglistapp/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/myshoppinglistapp/MainActivity.java
@@ -1,33 +1,37 @@
 package au.edu.jcu.sp3406.myshoppinglistapp;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends ListActivity {
-    ArrayList<TextView> list = new ArrayList<TextView>();
-    ArrayAdapter<TextView> adapter;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        setListAdapter(adapter);
-    }
-
-    public void addButton(View view) {
-        TextView text = findViewById(R.id.text);
-        list.add(text);
+        ListView listView = findViewById(R.id.list);
+        String[] list = new String[]{"test1", "test2"};
+        List<String> shopList = new ArrayList<String>(Arrays.asList(list));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, shopList);
+        listView.setAdapter(arrayAdapter);
 
     }
 
-    public void settingsButton(View view){
+    public void addButton(View view){
+        TextView textView = findViewById(R.id.text);
+        String testString = textView.toString();
+        //cant see list to add to it from button -- if I move list to main activity I can see it but app crashes.
 
     }
 }
