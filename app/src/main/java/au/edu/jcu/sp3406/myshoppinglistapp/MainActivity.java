10f2a25622b05b3e6e9a3f3a52a828f9112ff283
Danny <danny.connolly@my.jcu.edu.au>
2020-04-23 06:06:08 +1000

    Added to main activity

--- app/src/main/java/au/edu/jcu/sp3406/myshoppinglistapp/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/myshoppinglistapp/MainActivity.java
@@ -4,28 +4,46 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
-    ArrayList<TextView> list = new ArrayList<TextView>();
-    ArrayAdapter<TextView> adapter;
+
+    private ArrayAdapter<String> adapter;
+    //private ArrayList<TextView> list = new ArrayList<>();
+    private Button add;
+    private EditText text;
+    private Button settings;
+    private ListView shopList;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        setListAdapter(adapter);
+        //shopList = (ListView) findViewById(R.id.list);
+        text = (EditText) findViewById(R.id.text);
+        add = (Button) findViewById(R.id.add);
+        settings = (Button) findViewById(R.id.settings);
+
+        //adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        //shopList.setAdapter(adapter);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getItem = text.getText().toString();
+                adapter.add(getItem);
+                text.setText("");
+            }
+        });
     }
 
-    public void addButton(View view) {
-        TextView text = findViewById(R.id.text);
-        list.add(text);
-
-    }
+//    public void addButton(View view) {
+//        TextView text = findViewById(R.id.text);
+////        list.add(text);
+//    }
 
     public void settingsButton(View view){
 
